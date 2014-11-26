365a01ee9612abd14da7f757b400d0e1c5d90af7
Jaydeep <jaydeep.wagh@changer.nl>
2014-11-26 12:30:47 +0530

    Library is still being tested from the demo project. Removed some unused layout resources

---
+++ PolyPickerDemo/libpolypicker7/src/main/java/nl/changer/polypicker/CameraFragment.java
@@ -0,0 +1,99 @@
+
+package nl.changer.polypicker;
+
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import nl.changer.polypicker.model.Image;
+
+/**
+ * @deprecated
+ */
+public class CameraFragment extends Fragment implements Camera.ShutterCallback, Camera.PictureCallback {
+
+	private static final String TAG = CameraFragment.class.getSimpleName();
+
+	Camera mCamera;
+	ImageButton mTakePictureBtn;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Inflate the layout
+		// for this fragment_camera_cwac
+		View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+
+		mCamera = Camera.open();
+		CameraPreview preview = new CameraPreview(getActivity(), mCamera);
+
+		ViewGroup previewHolder = (ViewGroup) rootView.findViewById(R.id.preview_holder);
+		previewHolder.addView(preview);
+		// take picture even when the preview is clicked.
+		previewHolder.setOnClickListener(mOnTakePictureClicked);
+
+		mTakePictureBtn = (ImageButton) rootView.findViewById(R.id.take_picture);
+		mTakePictureBtn.setOnClickListener(mOnTakePictureClicked);
+		return rootView;
+	}
+
+	private View.OnClickListener mOnTakePictureClicked = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View view) {
+			if (mTakePictureBtn.isEnabled()) {
+				mTakePictureBtn.setEnabled(false);
+				if (mCamera != null) {
+					mCamera.takePicture(CameraFragment.this, null, CameraFragment.this);
+				}
+			}
+		}
+	};
+
+	@Override
+	public void onPictureTaken(byte[] bytes, Camera camera) {
+		mTakePictureBtn.setEnabled(true);
+		Bitmap picture = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+		// TODO: Rotating the image here is not really
+		// required. We can present the image as is in the preview
+		// at the bottom. We can make a provision in the library that the user
+		// can select an image and rotate it. This UX can be similar to
+		// what WhatsApp does when trying to add multiple images.
+		/*Matrix matrix = new Matrix();
+		matrix.postRotate(90);
+		picture = Bitmap.createBitmap(picture, 0, 0, picture.getWidth(), picture.getHeight(), matrix, true);*/
+
+		String path = MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), picture, "", "");
+		Uri contentUri = Uri.parse(path);
+		Image image = getImageFromContentUri(contentUri);
+		((ImagePickerActivity) getActivity()).addImage(image);
+
+		mCamera.startPreview();
+	}
+
+	public Image getImageFromContentUri(Uri contentUri) {
+
+		String[] cols = { MediaStore.Images.Media.DATA, MediaStore.Images.ImageColumns.ORIENTATION };
+
+		// can post image
+		Cursor cursor = getActivity().getContentResolver().query(contentUri, cols, null, null, null);
+		cursor.moveToFirst();
+		Uri uri = Uri.parse(cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)));
+		int orientation = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns.ORIENTATION));
+		return new Image(uri, orientation);
+	}
+
+	@Override
+	public void onShutter() {
+
+	}
+}
---
+++ PolyPickerDemo/libpolypicker7/src/main/java/nl/changer/polypicker/CameraPreview.java
@@ -0,0 +1,184 @@
+
+package nl.changer.polypicker;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.os.Build;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.List;
+
+/**
+ * @deprecated
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+	private static final String TAG = "CameraPreview";
+
+	private Context mContext;
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	// private List<Camera.Size> mSupportedPreviewSizes;
+	private Size mPreviewSize;
+
+	public CameraPreview(Context context, Camera camera) {
+		super(context);
+		mContext = context;
+		mCamera = camera;
+
+		// Install a SurfaceHolder.Callback so we get notified when the
+		// underlying surface is created and destroyed.
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		// deprecated setting, but required on Android versions prior to 3.0
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		// empty. surfaceChanged will take care of stuff
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		if (mCamera != null) {
+			mCamera.stopPreview();
+			mCamera.setPreviewCallback(null);
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+		// Log.e(TAG, "surfaceChanged => w=" + w + ", h=" + h);
+		// If your preview can change or rotate, take care of those events here.
+		// Make sure to stop the preview before resizing or reformatting it.
+		if (mHolder.getSurface() == null) {
+			// preview surface does not exist
+			Log.w(TAG, " The holder surface does not exist.");
+			return;
+		}
+
+		// stop preview before making changes
+		try {
+			if (mCamera != null) {
+				mCamera.stopPreview();
+			}
+		} catch (Exception e) {
+			// ignore: tried to stop a non-existent preview
+			e.printStackTrace();
+		}
+
+		// set preview size and make any resize, rotate or reformatting changes
+		// here
+		// start preview with new settings
+		try {
+			Camera.Parameters parameters = mCamera.getParameters();
+			parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+			if (mCamera != null) {
+				mCamera.setParameters(parameters);
+				mCamera.setDisplayOrientation(90);
+				mCamera.setPreviewDisplay(mHolder);
+				mCamera.startPreview();
+			}
+		} catch (Exception e) {
+			Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+		}
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+		final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+		// supported preview sizes
+		List<Size> supportedPreviewSizes = null;
+
+		if (mCamera != null) {
+			supportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+
+			for (Size str : supportedPreviewSizes) {
+				Log.i(TAG, str.width + "/" + str.height);
+			}
+		}
+
+		if (supportedPreviewSizes != null) {
+			// TODO: fix this.
+			// thisis a hack involved here for now.
+			// Was not able to figure out why resolveSize above is return height
+			// = 0 on my Nexus 4
+			mPreviewSize = getOptimalPreviewSize(supportedPreviewSizes, width, getResources().getDisplayMetrics().heightPixels);
+		}
+
+		float ratio = 0;
+
+		if (mPreviewSize != null) {
+			if (mPreviewSize.height >= mPreviewSize.width) {
+				ratio = (float) mPreviewSize.height / (float) mPreviewSize.width;
+			} else {
+				ratio = (float) mPreviewSize.width / (float) mPreviewSize.height;
+			}
+		}
+
+		Log.i(TAG, " width: " + mPreviewSize.width + " height: " + mPreviewSize.height);
+		// Toast.makeText(getContext(), " width: " + mPreviewSize.width + " height: " +
+		// mPreviewSize.height, Toast.LENGTH_SHORT).show();
+
+		if (ratio != 0) {
+			// One of these methods should be used,
+			// second method squishes preview slightly.
+			setMeasuredDimension(width, (int) (width * ratio));
+			// setMeasuredDimension((int) (width * ratio), height);
+		} else {
+			setMeasuredDimension(width, height);
+		}
+	}
+
+	private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+		Log.d(TAG, "values: " + w + "/" + h);
+		final double ASPECT_TOLERANCE = 0.1;
+		double targetRatio = (double) h / w;
+
+		if (sizes == null) {
+			return null;
+		}
+
+		Size optimalSize = null;
+		double minDiff = Double.MAX_VALUE;
+
+		int targetHeight = h;
+
+		for (Size size : sizes) {
+			double ratio = (double) size.height / size.width;
+			if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) {
+				continue;
+			}
+
+			if (Math.abs(size.height - targetHeight) < minDiff) {
+				optimalSize = size;
+				minDiff = Math.abs(size.height - targetHeight);
+			}
+		}
+
+		if (optimalSize == null) {
+			minDiff = Double.MAX_VALUE;
+			for (Size size : sizes) {
+				if (Math.abs(size.height - targetHeight) < minDiff) {
+					optimalSize = size;
+					minDiff = Math.abs(size.height - targetHeight);
+				}
+			}
+		}
+
+		// TODO: This is very hacky way to do device specific customization.
+		// Ideally CameraFragment from the library specified here should be used in future.
+		// https://github.com/commonsguy/cwac-camera
+		if (Build.MODEL != null && Build.MODEL.equalsIgnoreCase("GT-I9500")) {
+			optimalSize.width = 1008;
+			optimalSize.height = 566;
+		}
+
+		return optimalSize;
+	}
+}
